8f0a67fba53ffa009f5dfc312fbda2cd34dd06de
adaocjunior <adao8678@gmail.com>
2026-06-13 16:11:58 -0300

    Idades generalizadas

--- script.py
+++ script.py
@@ -3,3 +3,14 @@ import pandas as pd
 df = pd.read_csv('insurance.csv')
 
 df.head()
+
+#Classificando idade de forma generica
+df["age_group"] = pd.cut(
+    df["age"],
+    bins=[0,29,44,64],
+    labels=["18-29","30-44","45-64"]
+)
+
+df[["age","age_group"]].head()
+
+print(df[["age","age_group"]].head())
